8a60132241a9e04446ed84f8cdf559aa53f44a72
Santiago Garcia Arango <san99tiago@gmail.com>
2024-11-23 21:27:19 -0500

    Add AWS Config rules demo stack

--- config_rules_demo/stacks/config_rules_stack.py
+++ config_rules_demo/stacks/config_rules_stack.py
@@ -1,5 +1,6 @@
 # External imports
 from aws_cdk import (
+    aws_config,
     Stack,
     CfnOutput,
 )
@@ -43,8 +44,22 @@ class ConfigRulesStack(Stack):
         """
         Method to create AWS Config Rules.
         """
-        # TODO: Create multiple demo rules...
-        pass
+        # Demo rule 1: S3 Bucket Versioning is enabled
+        rule_1 = aws_config.ManagedRule(
+            self,
+            "DemoRule1",
+            config_rule_name="demo-rule-1-s3-versioning-enabled",
+            identifier=aws_config.ManagedRuleIdentifiers.S3_BUCKET_VERSIONING_ENABLED,
+            description="Demo rule 1: S3 Bucket Versioning is enabled",
+        )
+
+        rule_2 = aws_config.ManagedRule(
+            self,
+            "DemoRule2",
+            config_rule_name="demo-rule-2-s3-public-read-write-prohibited",
+            identifier=aws_config.ManagedRuleIdentifiers.S3_BUCKET_PUBLIC_READ_PROHIBITED,
+            description="Demo rule 2: S3 Bucket public read write prohibited",
+        )
 
     def generate_cloudformation_outputs(self) -> None:
         """
